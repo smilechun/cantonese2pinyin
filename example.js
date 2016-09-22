7616a8c0312dba6ea1a87b28bc65e447495ac0ed
Chun Fat, Tai <chunfat@setsailsoftware.com>
2016-09-22 14:52:54 +0800

    added example

---
+++ example.js
@@ -0,0 +1,18 @@
+/**
+ * Created by chunfat on 9/22/16.
+ */
+
+// Return null when not found
+console.log(getCodePoint(19958));
+console.log(getPinyin(199990));
+console.log(getChar("一"));
+
+
+// Return a matched Array object
+console.log(getCodePoint("一"));
+console.log(getPinyin("一"));
+console.log(getPinyin(19968));
+console.log(getChar("yat"));
+console.log(getChar(19968));
+
+
